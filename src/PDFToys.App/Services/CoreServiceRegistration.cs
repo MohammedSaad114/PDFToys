9ba224d30217f737e9a3cace9d90b7e24521b5d0
Mohammed Saad <mosa00004@stud.uni-saarland.de>
2026-06-27 00:03:10 +0200

    PDFTS-23 Add core service registration

---
+++ src/PDFToys.App/Services/CoreServiceRegistration.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using PDFToys.Core.Contracts;
+using PDFToys.Core.OS.Windows;
+using PDFToys.Core.Services;
+using PDFToys.Core.Services.ExportStrategies;
+using PDFToys.Core.Services.ImageProcessing;
+using PDFToys.Core.Services.ConversionStrategies;
+
+namespace PDFToys.App.Services;
+
+public static class CoreServiceRegistration
+{
+    public static IServiceCollection AddPdfToysCore(this IServiceCollection services)
+    {
+        services.AddSingleton<IImageCompressor, SkiaImageCompressor>();
+        services.AddSingleton<IConversionStrategy, ImageToPdfStrategy>();
+        services.AddSingleton<IConversionStrategy, TextToPdfStrategy>();
+        services.AddSingleton<IConversionStrategy, SvgToPdfStrategy>();
+        services.AddSingleton<IConversionStrategy, OfficeComPdfConverter>();
+        services.AddSingleton<IExportStrategy, ImageExportStrategy>();
+        services.AddSingleton<IExportStrategy, MarkdownExportStrategy>();
+
+        services.AddSingleton<IMergeService, MergeService>();
+        services.AddSingleton<ISplitService, SplitService>();
+        services.AddSingleton<ICompressionService, CompressionService>();
+        services.AddSingleton<IProtectService, ProtectService>();
+        services.AddSingleton<IRearrangeService, RearrangeService>();
+        services.AddSingleton<IConversionService, ConversionService>();
+        services.AddSingleton<IExportService, ExportService>();
+
+        return services;
+    }
+}
