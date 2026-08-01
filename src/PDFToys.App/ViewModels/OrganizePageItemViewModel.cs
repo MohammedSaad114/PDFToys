2635a8145c2eae6a2c4c672aee03dd367d2a659a
Mohammed Saad <mosa00004@stud.uni-saarland.de>
2026-08-01 19:38:17 +0200

    PDFTS-32 Add organize page item view model

---
+++ src/PDFToys.App/ViewModels/OrganizePageItemViewModel.cs
@@ -0,0 +1,68 @@
+﻿namespace PDFToys.App.ViewModels;
+
+public sealed class OrganizePageItemViewModel : ViewModelBase
+{
+    private int _rotationDegrees;
+    private bool _isIncluded = true;
+    private int _displayPosition;
+
+    public OrganizePageItemViewModel(int sourcePageNumber, int displayPosition)
+    {
+        SourcePageNumber = sourcePageNumber;
+        _displayPosition = displayPosition;
+    }
+
+    public int SourcePageNumber { get; }
+
+    public int DisplayPosition
+    {
+        get => _displayPosition;
+        set
+        {
+            if (_displayPosition == value)
+            {
+                return;
+            }
+
+            _displayPosition = value;
+            OnPropertyChanged();
+        }
+    }
+
+    public int RotationDegrees
+    {
+        get => _rotationDegrees;
+        set
+        {
+            var normalized = ((value % 360) + 360) % 360;
+            if (_rotationDegrees == normalized)
+            {
+                return;
+            }
+
+            _rotationDegrees = normalized;
+            OnPropertyChanged();
+            OnPropertyChanged(nameof(RotationLabel));
+        }
+    }
+
+    public string RotationLabel => $"{RotationDegrees}°";
+
+    public bool IsIncluded
+    {
+        get => _isIncluded;
+        set
+        {
+            if (_isIncluded == value)
+            {
+                return;
+            }
+
+            _isIncluded = value;
+            OnPropertyChanged();
+            OnPropertyChanged(nameof(IncludedLabel));
+        }
+    }
+
+    public string IncludedLabel => IsIncluded ? "Yes" : "No";
+}
