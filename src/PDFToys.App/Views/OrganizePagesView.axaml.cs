afe25954abac5dfcef4a04ca5ae1ab587c07d8dc
Mohammed Saad <mosa00004@stud.uni-saarland.de>
2026-08-02 22:03:21 +0200

    PDFTS-33 added pages organization and preview ui

---
+++ src/PDFToys.App/Views/OrganizePagesView.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace PDFToys.App.Views;
+
+public partial class OrganizePagesView : UserControl
+{
+    public OrganizePagesView()
+    {
+        InitializeComponent();
+    }
+}
---
+++ src/PDFToys.App/Views/PagePreviewWindow.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace PDFToys.App.Views;
+
+public partial class PagePreviewWindow : Window
+{
+    public PagePreviewWindow()
+    {
+        InitializeComponent();
+    }
+}
