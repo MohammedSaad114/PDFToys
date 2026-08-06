e780a94e1e878af71462c1fa3e7af3570fabb94c
Mohammed Saad <mosa00004@stud.uni-saarland.de>
2026-08-06 23:23:00 +0200

    PDFTS-41 Add user settings model

---
+++ src/PDFToys.App/Models/UserSettings.cs
@@ -0,0 +1,10 @@
+﻿namespace PDFToys.App.Models;
+
+public sealed class UserSettings
+{
+    public string? LastWorkspace { get; set; }
+
+    public string DefaultPdfOutputMode { get; set; } = nameof(PdfOutputMode.CreateNewCopy);
+
+    public string DefaultCompressionLevel { get; set; } = "Standard";
+}
---
+++ src/PDFToys.App/Services/IUserSettingsStore.cs
@@ -0,0 +1,10 @@
+﻿using PDFToys.App.Models;
+
+namespace PDFToys.App.Services;
+
+public interface IUserSettingsStore
+{
+    UserSettings Load();
+
+    void Save(UserSettings settings);
+}
